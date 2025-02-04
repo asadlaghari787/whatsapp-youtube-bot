b05f8f2ebe4b247d92900aaf65a685c97c01d9cf
asadlaghari787 <114422931+asadlaghari787@users.noreply.github.com>
2025-02-04 10:49:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function startBot() {
 const { makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys");
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 async function startBot() {
     const { state, saveCreds } = await useMultiFileAuthState("auth_info_baileys");
